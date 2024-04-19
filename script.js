869621b1a742bb41c63b5a75c6d6c9b7474ee165
laetitia piat <laeptitchat29@gmail.com>
2024-04-19 14:32:38 +0200

    style + div main

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const modal = document.querySelector(".modal-container")
 
 const btn = document.querySelector(".btn-modal")
+const main = document.querySelector("#main")
+
 
 btn.addEventListener('click', function(){
     console.log("click!");
     modal.style.top = "-100%";
     modal.style.width = "0%";
+    main.style.display = "block"
+
 });
